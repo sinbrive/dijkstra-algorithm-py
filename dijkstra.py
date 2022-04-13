54c2af633a155bc50df9ca8af4263fa8af70458e
sinbrive <21102151+sinbrive@users.noreply.github.com>
2022-04-13 10:00:54 +0000

    commented

--- dijkstra.py
+++ dijkstra.py
@@ -1,4 +1,5 @@
 
+# directed weighted graph
 graph = {'a':{'b':10,'c':3},
          'b':{'c':1,'d':2},
          'c':{'b':4,'d':8,'e':2},
@@ -6,6 +7,7 @@ graph = {'a':{'b':10,'c':3},
          'e':{'d':9}
         }
 
+# undirected weighted graph (SNT example)
 graph2 = {'a':{'b':8,'c':6.5, 'i':6.7, 'f':7},
          'b':{'c':6.5},
          'c':{'d':4},
@@ -14,38 +16,41 @@ graph2 = {'a':{'b':8,'c':6.5, 'i':6.7, 'f':7},
          'f':{'g':2.5},
          'g':{'h':4.5, 'i':5.5},
          'h':{'j':10},
-         'i':{'d':4, 'h':7, 'g':5.5},
+         'i':{'h':7},
          'j':{'h':10, 'e':10}
         }
 
 def dijkstra(graph,start,goal):
     shortest_distance = {}
     predecessor = {}
-    nonVisited = graph
+    unVisited = graph
     infinity = 9999999
     path = []
-    for node in nonVisited:
+  
+    # initialisation : 0 for start and infinity for other
+    for node in unVisited:
         shortest_distance[node] = infinity
     shortest_distance[start] = 0
 
-    while nonVisited:
+    # go through unvisited nodes
+    while unVisited:
         focusNode = None
-        for node in nonVisited:
+        # select focus node
+        for node in unVisited:
             if focusNode is None:
                 focusNode = node
             elif shortest_distance[node] < shortest_distance[focusNode]:
                 focusNode = node
-
-        for childNode, cost in graph[focusNode].items():
-            if cost + shortest_distance[focusNode] < shortest_distance[childNode]:
-                shortest_distance[childNode] = cost + shortest_distance[focusNode]
+        # for this focus node, check each children weight (= distance(focus, child))
+        for childNode, weight in graph[focusNode].items():
+            if weight + shortest_distance[focusNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[focusNode]
                 predecessor[childNode] = focusNode
-        nonVisited.pop(focusNode)
+        # don't come back anymore to this focusNode
+        unVisited.pop(focusNode)
       
-    print(str(shortest_distance))
-    print()
-    print(str(predecessor))
-    print()
+    print('distance from start:'+str(shortest_distance), '\n')
+    print('predecessors:'+str(predecessor), '\n')
 
     currentNode = goal
     while currentNode != start:
@@ -57,8 +62,8 @@ def dijkstra(graph,start,goal):
             break
     path.insert(0,start)
     if shortest_distance[goal] != infinity:
-        print('Shortest distance is ' + str(shortest_distance[goal]))
-        print('And the path is ' + str(path))
+        print('Shortest distance is ' + str(shortest_distance[goal]), '\n')
+        print('The path is ' + str(path))
 
 
 dijkstra(graph2, 'a', 'j')
